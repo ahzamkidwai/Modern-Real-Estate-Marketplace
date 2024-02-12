e850326180889792edaa249e72d15e9892bd1272
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2024-02-12 11:06:00 +0530

    Complete Listing Page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -9,7 +9,6 @@ import { app } from "../firebase";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-
 function CreateListing() {
   const { currentUser } = useSelector((state) => state.user);
   const navigate = useNavigate();
@@ -332,7 +331,10 @@ function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs ">($/month)</span>
+                <span className="text-xs ">($ / month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
             <div className="flex items-center gap-2">
@@ -350,6 +352,9 @@ function CreateListing() {
               <div className="flex flex-col items-center">
                 <p>Discount Price</p>
                 <span className="text-xs ">($/month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
           </div>
